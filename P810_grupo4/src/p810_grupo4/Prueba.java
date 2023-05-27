21b7ddf50fa5604dd81fa4847e1e01a7d10fefcd
marina castro <marina castro@LAPTOP-R16J7I4O>
2023-05-27 12:42:12 +0200

    Añadimos vehiculos para hacer las pruebas

--- P810_grupo4/src/p810_grupo4/Prueba.java
+++ P810_grupo4/src/p810_grupo4/Prueba.java
@@ -4,7 +4,17 @@ public class Prueba {
 
 
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Concesionario concesionario = new Concesionario();
+
+        Vehiculo vehiculo1 = new Vehiculo("seat", "panda", 2003, 8000);
+        Vehiculo vehiculo2 = new Vehiculo("mercedes", "c", 2016, 30000);
+        Vehiculo vehiculo3 = new Vehiculo("kia", "niro", 2020, 23000);
+
+        concesionario.agregarVehiculo(vehiculo1);
+        concesionario.agregarVehiculo(vehiculo2);
+        concesionario.agregarVehiculo(vehiculo3);
+
     }
     
 }
