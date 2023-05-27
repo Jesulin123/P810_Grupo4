2319f9ddfbbfef74d7ad16701c9e38d2686aeb17
Jesulin123 <132340308+Jesulin123@users.noreply.github.com>
2023-05-27 22:13:48 +0200

    Actualizado clase Cliente

--- P810_grupo4/src/p810_grupo4/Cliente.java
+++ P810_grupo4/src/p810_grupo4/Cliente.java
@@ -1,17 +1,38 @@
 package p810_grupo4;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
-	public boolean agregar;
+public class Cliente implements Comparable<Cliente> {
+	private String nombre;
+	private String direccion;
+	private List<Vehiculo> vehiculosComprados;
 
-	public Cliente(String string) {
-		// TODO Auto-generated constructor stub
+	public Cliente(String nombre, String direccion) {
+		this.nombre = nombre;
+		this.setDireccion(direccion);
+		this.vehiculosComprados = new ArrayList<>();
 	}
 
-	public void agregarVehiculoComprado(Vehiculo vehiculo) {
-		// TODO Auto-generated method stub
-		
+	public String getDireccion() {
+		return direccion;
 	}
 
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	@Override
+	public int compareTo(Cliente otro) {
+		return this.nombre.compareTo(otro.nombre);
+	}
+
+	public void comprarVehiculo(Vehiculo vehiculo) {
+		this.vehiculosComprados.add(vehiculo);
+	}
+
+	public List<Vehiculo> getVehiculosComprados() {
+		return this.vehiculosComprados;
+	}
 
 }
