272fc3f04d55f2de294c300d48e7ce902f74fd4c
Jesulin123 <132340308+Jesulin123@users.noreply.github.com>
2023-05-27 22:12:46 +0200

    Actualización clase Camion

--- P810_grupo4/src/p810_grupo4/Camion.java
+++ P810_grupo4/src/p810_grupo4/Camion.java
@@ -1,13 +1,18 @@
 package p810_grupo4;
 
+public class Camion extends Vehiculo {
+	private double capacidadCarga;
 
-public class Camion extends Vehiculo{
-	
-	public Camion(String marca, String modelo, int año, int precio) {
-		super(marca, modelo, año, precio);
-		// TODO Auto-generated constructor stub
+	public Camion(String marca, String modelo, int ano, double precio, double capacidadCarga) {
+		super(marca, modelo, ano, precio);
+		this.capacidadCarga = capacidadCarga;
 	}
 
-	private int capacidadCarga;
-	
+	public double getCapacidadCarga() {
+		return capacidadCarga;
+	}
+
+	public void setCapacidadCarga(double capacidadCarga) {
+		this.capacidadCarga = capacidadCarga;
+	}
 }
